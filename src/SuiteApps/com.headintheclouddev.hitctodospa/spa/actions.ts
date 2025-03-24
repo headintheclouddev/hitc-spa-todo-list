509f743e244430f69bfb5ef356443ea56ec78df0
Robin Mitchell <robin.mitchell@headintheclouddev.com>
2025-03-24 14:52:05 +0000

    Implement placeholder state management

--- src/SuiteApps/com.headintheclouddev.hitctodospa/spa/actions.ts
+++ src/SuiteApps/com.headintheclouddev.hitctodospa/spa/actions.ts
@@ -9,5 +9,27 @@ export const ActionType = {
 
 // Action creator functions handle possible side effects (such as CRUD operations), so that the reducer function can be pure.
 export const Action = {
-
+  addToDo(title: string) {
+    const newId = '0'; // TODO: Create task record in NetSuite
+    return { type: ActionType.ADD_TODO, newId, title };
+  },
+  fetchToDo() {
+    // TODO: Look up tasks from NetSuite
+    const todos = [
+      { title: "Create placeholder components", completed: true,  id: '1' },
+      { title: "Implement state management",    completed: false, id: '2' }
+    ];
+    return { type: ActionType.FETCH_TODOS, todos };
+  },
+  toggleToDo(id: string) {
+    // TODO: Update task status in NetSuite
+    return { type: ActionType.TOGGLE_TODO, id };
+  },
+  removeToDo(id: string) {
+    // TODO: Delete task record in NetSuite
+    return { type: ActionType.REMOVE_TODO, id };
+  },
+  filterToDos(filter: string) {
+    return { type: ActionType.FILTER_TODO, filter };
+  }
 }
--- src/SuiteApps/com.headintheclouddev.hitctodospa/spa/reducers.ts
+++ src/SuiteApps/com.headintheclouddev.hitctodospa/spa/reducers.ts
@@ -1,5 +1,41 @@
+import { ActionType } from './actions';
+import { IToDo } from './TodoItem';
+
 // Reducers take a state, apply an action, and return a new state
 
-export default function appReducer(state: any, action: any) {
-  return { ...state };
+export default function appReducer(state: { todos: IToDo[], filter: string }, action: any) {
+  return {
+    todos: todosReducer(state.todos, action),
+    filter: filterReducer(state.filter, action)
+  }
+}
+
+function filterReducer(state: string, action: { type: Symbol, filter: string }) { // Here, state is the current filter and action.filter is the new filter
+  if (action.type == ActionType.FILTER_TODO) {
+    return action.filter;
+  } else {
+    return state;
+  }
+}
+
+function todosReducer(state: IToDo[], action: { type: Symbol, title?: string, id?: string, todos?: IToDo[] }) {
+  switch (action.type) {
+    case ActionType.FETCH_TODOS:
+      return action.todos;
+    case ActionType.ADD_TODO: // The task has already been created in NetSuite; here we're just adding it to the app state
+      const newTodo = { id: action.id, title: action.title, completed: false };
+      return [newTodo, ...state];
+    case ActionType.TOGGLE_TODO:
+      return state.map(t => { // Iterate through all the todos in the list and just reverse the completed status on the one selected
+        if (t.id == action.id)
+          return { ...t, completed: !t.completed };
+        return t;
+      }, []);
+    case ActionType.REMOVE_TODO:
+      return state.filter(t => { // Return all the todos except for the one we're removing
+        return t.id != action.id;
+      });
+    default:
+      return state;
+  }
 }
