4aba8419076468f5fed5526967dbc7999c3edde8
Robin Mitchell <robin.mitchell@headintheclouddev.com>
2025-03-28 11:34:18 +0000

    Add Todo: Fill in state management hooks

--- src/SuiteApps/com.headintheclouddev.hitctodospa/spa/AddTodo.tsx
+++ src/SuiteApps/com.headintheclouddev.hitctodospa/spa/AddTodo.tsx
@@ -1,12 +1,27 @@
-import { JSX, VDom } from '@uif-js/core';
+import { JSX, VDom, useState, useDispatch } from '@uif-js/core';
 import {Button, TextBox} from "@uif-js/component";
+import { Action } from './actions';
 
 export default function AddTodo(): JSX.Element {
+  const [input, setInput] = useState('');
+  const dispatch = useDispatch();
+
+  function handleInput(evt: TextBox.TextChangedArgs): void {
+    setInput(evt.text);
+  }
+
+  function handleAdd(): void {
+    if (input) {
+      dispatch(Action.addToDo(input));
+      setInput('');
+    }
+  }
+
   return (
     <VDom.Fragment>
       <div width="300px">
-        <TextBox type={TextBox.Type.TEXT} placeholder="Enter new task..." />
-        <Button label="Add" />
+        <TextBox type={TextBox.Type.TEXT} placeholder="Enter new task..." onTextChanged={handleInput} text={input} />
+        <Button label="Add" action={handleAdd} />
       </div>
     </VDom.Fragment>
   )
