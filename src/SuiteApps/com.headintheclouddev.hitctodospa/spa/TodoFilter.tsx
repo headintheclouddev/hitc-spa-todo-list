5e20670867da6aabe5552f88a90c314d07e1cda5
Robin Mitchell <robin.mitchell@headintheclouddev.com>
2025-03-28 11:40:50 +0000

    Todo Filter: Add state management hooks and styling

--- src/SuiteApps/com.headintheclouddev.hitctodospa/spa/TodoFilter.tsx
+++ src/SuiteApps/com.headintheclouddev.hitctodospa/spa/TodoFilter.tsx
@@ -2,7 +2,17 @@ import {Action} from "./actions";
 import { JSX, useDispatch, useSelector } from "@uif-js/core";
 
 function TodoFilterItem(props: { name: string }): JSX.Element { // This represents a single filter option (all, active, or completed)
-  return <span>{props.name}</span>
+  const dispatch = useDispatch();
+  const filter = useSelector((state: { filter: string }) => state.filter); // This is the currently set filter
+
+  function handleFilter(): void {
+    console.log('handleFilter', props.name);
+    dispatch(Action.filterToDos(props.name));
+  }
+
+  const style = { color: 'blue', cursor: 'pointer', fontWeight: filter == props.name ? 'bold' : 'normal' };
+
+  return <span style={style} onClick={handleFilter}>{props.name}</span>
 }
 
 export default function TodoFilter(): JSX.Element { // Show three filter options, separated by a slash: /
